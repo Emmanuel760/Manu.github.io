ff4151bb7433a8e17ab1075aecd7c04c1395c83d
Emmanuel760 <ekiprotich834@gmail.com>
2022-07-07 10:16:47 +0300

    new project

---
+++ js/index.js
@@ -0,0 +1,13 @@
+ console.log("hello world");
+ var a=50
+ var b=35
+ var c=a+b
+ console.log(c);
+ const x=10
+ const w=25
+ const v=w*x
+ console.log(v);
+ var j="Today I ate bread "
+ var k="with a cup of tea"
+ var m= j+k
+ console.log(m);
---
+++ js/main.js
@@ -0,0 +1,68 @@
+var num1=35;
+var num2=40;
+if (num2 <num1) {
+    console.log(true);
+    
+}
+else{console.log(false
+    );}
+    const num3=15
+    if (num3>15) {console.log('positive number');
+        
+    }
+    else if (num3<15) {console.log('negative number ');
+        
+    }
+    else if(num3===15){console.log(15);}
+    
+ 
+ 
+    
+
+ let marks=1000
+ if (marks>=0 && marks<=32) {console.log("F");
+    
+ }
+ else if (marks>=33 && marks<=39) {console.log("D-");
+    
+ }
+ else if (marks>=39 && marks<=46) {console.log("D");
+    
+ }
+ else if (marks>=47 && marks<=53) {console.log("D+");
+    
+ }
+ else if (marks>=54 && marks<=60) {console.log("C-");
+    
+ }
+ else if (marks>=61 && marks<=67) {console.log("C+");
+    
+ }
+ else if (marks>=68 && marks<=74){console.log("B-");
+    
+ }
+ else if (marks>=75 && marks<=81) {console.log("B");
+    
+ }
+ else if (marks>=82 && marks<=88) {console.log("B+");
+    
+ }
+ else if (marks>=89 && marks<=95) {console.log("A-");
+    
+ }
+ else if (marks>=95 && marks<=100) {console.log("A");
+    
+ }
+ 
+ else{console.log("invalid number");}
+ 
+
+var cities=['nairobi','kampala','cape town','oslo','paris']
+cities.unshift('dakar','london')
+cities.push('cairo','mumbai')
+console.log(cities);
+var schools=['kamusinga','Alliance','Kabarak','Sacho','Kakamega']
+for (let data in schools ) 
+{console.log(data,schools[data]);
+        
+}
